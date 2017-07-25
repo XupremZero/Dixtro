1c4ea251db7b643da1c8bc6c4e17e6c8c9906afc
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-07-25 20:44:11 +0200

    Add files via upload

---
+++ Types.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+#include <cstdbool>
+#include <cstdarg>
+
+typedef void Void;
+typedef void* Delegate;
+typedef bool Boolean;
+typedef int8_t Byte;
+typedef uint8_t UByte;
+typedef int16_t Short;
+typedef uint16_t UShort;
+typedef int32_t Int;
+typedef uint32_t UInt;
+typedef int64_t Long;
+typedef uint64_t ULong;
+typedef float Float;
+typedef double Double;
+#define GetSize sizeof
+#define Reflective
